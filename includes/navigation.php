bd93da253607c518e05683fd137ea9a37561881f
daniel <daniel.deneef@gmail.com>
2024-10-12 16:49:46 +0200

    add the admin links to the navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,16 +1,34 @@
 <link rel="stylesheet" href="http://localhost/myTcg/styles/navigation.css">
 <nav id="sidebar">
-  <h2 class="text-center">User Dashboard</h2>
-  <ul class="nav flex-column">
-    <li class="nav-item">
-      <a class="nav-link" href="http://localhost/myTcg/">Dashboard</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="http://localhost/myTcg/pages/collection.php">Collection</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="http://localhost/myTcg/pages/account.php">Account Management</a>
-    </li>
-  </ul>
-  <button class="btn logout-btn mt-4" onclick="logout()">Logout</button>
+    <!-- admin links -->
+    <h2 class="text-center">Admin Dashboard</h2>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/pages/collectionMng.php">Collection</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/pages/users.php">user Management</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/pages/account.php">Account Management</a>
+        </li>
+    </ul>
+
+    <!-- user links -->
+    <h2 class="text-center">User Dashboard</h2>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/">Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/pages/collection.php">Collection</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/myTcg/pages/account.php">Account Management</a>
+        </li>
+    </ul>
+    <button class="btn logout-btn mt-4" onclick="logout()">Logout</button>
 </nav>
