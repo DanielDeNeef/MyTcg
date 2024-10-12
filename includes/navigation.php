db9651be6aa6fb64d447ca2610e54afca2e2e48a
daniel <daniel.deneef@gmail.com>
2024-10-12 17:05:44 +0200

    change hardcoded baseUrl

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,19 +1,24 @@
-<link rel="stylesheet" href="http://localhost/myTcg/styles/navigation.css">
+<?php 
+    $config = require dirname(__DIR__) . '/config/app.php'; 
+    $baseUrl = $config['url']["baseUrl"];
+?>
+
+<link rel="stylesheet" href=<?php echo $baseUrl .'styles/navigation.css' ?> />
 <nav id="sidebar">
     <!-- admin links -->
     <h2 class="text-center">Admin Dashboard</h2>
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/">Dashboard</a>
+            <a class="nav-link" href=<?php echo $baseUrl ?> >Dashboard</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/pages/collectionMng.php">Collection</a>
+            <a class="nav-link" href=<?php echo $baseUrl .'pages/collectionMng.php' ?>>Collection</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/pages/users.php">user Management</a>
+            <a class="nav-link" href=<?php echo $baseUrl .'pages/user.php' ?>>user Management</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/pages/account.php">Account Management</a>
+            <a class="nav-link" href=<?php echo $baseUrl .'pages/account.php' ?>>Account Management</a>
         </li>
     </ul>
 
@@ -21,13 +26,13 @@
     <h2 class="text-center">User Dashboard</h2>
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/">Dashboard</a>
+            <a class="nav-link" href=<?php echo $baseUrl ?>>Dashboard</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/pages/collection.php">Collection</a>
+            <a class="nav-link" href=<?php echo $baseUrl .'pages/collection.php' ?>>Collection</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="http://localhost/myTcg/pages/account.php">Account Management</a>
+            <a class="nav-link" href=<?php echo $baseUrl .'pages/account.php' ?>>Account Management</a>
         </li>
     </ul>
     <button class="btn logout-btn mt-4" onclick="logout()">Logout</button>
