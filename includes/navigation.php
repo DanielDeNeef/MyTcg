04dde3351a98a2e748d2abd17cdda4ce3d9806ab
daniel <daniel.deneef@gmail.com>
2024-10-12 17:25:16 +0200

    add logic to display admin or user content

--- includes/navigation.php
+++ includes/navigation.php
@@ -5,23 +5,29 @@
 
 <link rel="stylesheet" href=<?php echo $baseUrl .'styles/navigation.css' ?> />
 <nav id="sidebar">
-    <!-- admin links -->
-    <h2 class="text-center">Admin Dashboard</h2>
-    <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link" href=<?php echo $baseUrl ?> >Dashboard</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href=<?php echo $baseUrl .'pages/collectionMng.php' ?>>Collection</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href=<?php echo $baseUrl .'pages/user.php' ?>>user Management</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href=<?php echo $baseUrl .'pages/account.php' ?>>Account Management</a>
-        </li>
-    </ul>
 
+    <?php 
+        if($isAdmin){
+    ?>
+        <!-- admin links -->
+        <h2 class="text-center">Admin Dashboard</h2>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href=<?php echo $baseUrl ?> >Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=<?php echo $baseUrl .'pages/collectionMng.php' ?>>Collection</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=<?php echo $baseUrl .'pages/user.php' ?>>user Management</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=<?php echo $baseUrl .'pages/account.php' ?>>Account Management</a>
+            </li>
+        </ul>
+    <?php
+        }else{
+    ?>
     <!-- user links -->
     <h2 class="text-center">User Dashboard</h2>
     <ul class="nav flex-column">
@@ -35,5 +41,9 @@
             <a class="nav-link" href=<?php echo $baseUrl .'pages/account.php' ?>>Account Management</a>
         </li>
     </ul>
+
+    <?php 
+        }
+    ?>
     <button class="btn logout-btn mt-4" onclick="logout()">Logout</button>
 </nav>
