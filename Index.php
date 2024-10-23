29fcd74c2068da34fad8fa1f6b14c488f445bdd8
daniel <daniel.deneef@gmail.com>
2024-10-23 21:03:20 +0200

    refactor the adminDashboard

--- Index.php
+++ Index.php
@@ -1,15 +1,8 @@
 <?php 
-    // session_start();
     include 'includes/dbconnect.php';
     include 'includes/controlLogin.php';
     include 'includes/header.php';
     include 'includes/dashboardServices.php';
-
-    // Fetch the total counts
-    $gameCount = getTotalGames();
-    $gameSetCount = getTotalGameSets();
-    $cardCount = getTotalCards();
-    $userCount = getTotalUsers();
 ?>
 
 <link rel="stylesheet" href="styles/main.css">
@@ -17,56 +10,12 @@
 <?php include 'includes/navigation.php'; ?>
 
 <div id="content">
-    <div class="container mt-5">
-        <!-- Dashboard Section -->
-        <section id="dashboard">
-            <h1>Dashboard</h1>
-            <p>Here is an overview of the system data.</p>
+<?php 
 
-            <div class="row g-2">
-                <!-- Game Count Card -->
-                <div class="col-12 col-sm-6 col-lg-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Games</h5>
-                            <p class="card-text display-4"><?= $gameCount ?></p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Game Set Count Card -->
-                <div class="col-12 col-sm-6 col-lg-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Game Sets</h5>
-                            <p class="card-text display-4"><?= $gameSetCount ?></p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Card Count Card -->
-                <div class="col-12 col-sm-6 col-lg-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Cards</h5>
-                            <p class="card-text display-4"><?= $cardCount ?></p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- User Count Card -->
-                <div class="col-12 col-sm-6 col-lg-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Users</h5>
-                            <p class="card-text display-4"><?= $userCount ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <!-- Include the footer -->
-        <?php include 'includes/footer.php'; ?>
-    </div>
+    // display the admin dashboard else display the user dashboard
+    if ($isAdmin) {
+        include 'includes/adminDashboard.php';
+    }
+    
+?>
 </div>
